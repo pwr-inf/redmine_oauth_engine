3d5122d2320d3ae6e06ec3ef01e2453c265b34d7
Piotr Szymanski <niedakh@gmail.com>
2016-08-20 13:03:47 +0200

    - autologin support

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,2 @@
-get 'oauth_engine', :to => 'redmine_oauth#oauth_engine'
+get 'oauth_engine', :to => 'redmine_oauth#oauth_engine'#REM, :as => 'signin'
 get '_oauth2engine', :to => 'redmine_oauth#oauth_engine_callback', :as => 'oauth_engine_callback'
